c8fac1ddb60bf06ecd7c07f297145b839d6c456a
b-ivo <ivobryan12@gmail.com>
2026-08-13 07:19:22 +0200

    Simple fixe

--- server/Server.js
+++ server/Server.js
@@ -11,7 +11,7 @@ import daysRouter from "./routes/days.js";
 setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
